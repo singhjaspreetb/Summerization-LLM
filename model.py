c2bd49d8f6839a617d23123aa8020e8a91e192a7
Jaspreet Singh <harshitbhattal@gmail.com>
2023-04-14 15:32:11 +0530

    update

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
@@ -7,35 +7,25 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 import os
 
-kay = st.text_input('Your Key',placeholder="Enter Your key")
-os.environ["OPENAI_API_KEY"] = kay
-
-
 st.title('Summerisation Model')
 
-temp = st.slider('Temprature', 1, 5, 1)
-st.write("Temprature ", temp)
+api_key = st.text_input('Your Key',placeholder="Enter Your key")
+os.environ["OPENAI_API_KEY"] = api_key
 
 length = st.slider('Max Characters', 200, 4000, 200)
 st.write("Characters ", length)
 
 raw_text = ''
 
-# inp = st.radio(
-#     "Select Input Format",
-#     ('PDF', 'Text'))
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    reader = PdfReader(bytes_data)
+    for i, page in enumerate(reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
 
-# # if inp == 'PDF':
-# uploaded_file = st.file_uploader("Choose a CSV file", type="pdf")
-# if uploaded_file is not None:
-#     bytes_data = uploaded_file.getvalue()
-#     reader = PdfReader(bytes_data)
-#     for i, page in enumerate(reader.pages):
-#         text = page.extract_text()
-#         if text:
-#             raw_text += text
-
-# else:
 temp_data = st.text_area('Text to analyze',placeholder="Eneter Your Data")
 
 text_splitter = CharacterTextSplitter(
@@ -45,20 +35,19 @@ text_splitter = CharacterTextSplitter(
     length_function = len,
 )
 
-# texts = text_splitter.split_text(raw_text)
-# texts.append(temp_data)
-texts = [temp_data]
+texts = text_splitter.split_text(raw_text)
+texts.append(temp_data)
 
 query = st.text_area('Query on Data',placeholder="Enter Your Query")
 
 if st.button('Submit'):
     embeddings = OpenAIEmbeddings()
     docsearch = FAISS.from_texts(texts, embeddings)
-    chain = load_qa_chain(OpenAI(), chain_type="stuff")
+    chain = load_qa_chain(OpenAI(max_tokens=int(length),model_name="gpt-3.5-turbo"), chain_type="stuff")
 
-    docs = docsearch.similarity_search(query)    
-    st.write('Output :', docs)
-    # st.write('Summery :', txt)
+    docs = docsearch.similarity_search(query)  
+    txt=chain.run(input_documents=texts, question="Summerize within 150 words")
+    st.write('Summery :', txt)
     txt=chain.run(input_documents=docs, question=query)
     st.write('Output :', txt)
 else:
